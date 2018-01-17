f8ff8c71fa3cc54806971682e4ab10a6081b8dc8
Erlemorgaine <erle_3@hotmail.com>
2018-01-17 17:51:18 +0100

    Add Model Specs: Associations

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -34,6 +34,30 @@ RSpec.describe Event, type: :model do
         expect(Event.order_by_price).to eq(ordered)
       end
 
+      describe "association with user" do
+        let(:user) { create :user }
+
+        it "belongs to a user" do
+          event = user.events.build(event_type: "Art")
+
+          expect(event.user).to eq(user)
+        end
+      end
+
+      describe "association with category" do
+        let(:event) { create :event }
+
+        let(:category1) { create :category, name: "Bright", events: [event] }
+        let(:category2) { create :category, name: "Clean lines", events: [event] }
+        let(:category3) { create :category, name: "A Man's Touch", events: [event] }
+
+        it "has category" do
+          expect(event.categories).to include(category1)
+          expect(event.categories).to include(category2)
+          expect(event.categories).to include(category3)
+        end
+      end
+
     end
 
   end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  describe "association with event" do
+    let!(:user) { create :user }
+    let!(:event) { create :event, user: user }
+
+    it "has many events" do
+      event1 = user.events.new(name: "Concert")
+      event2 = user.events.new(name: "Festival")
+
+      expect(user.events).to include(event1)
+      expect(user.events).to include(event2)
+    end
+
+    it "deletes associated events" do
+      expect { user.destroy }.to change(Event, :count).by(-1)
+    end
+  end
+end
