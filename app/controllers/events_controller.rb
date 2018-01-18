a92ba9b49e11eec4db0cf8a7d950c8538e818074
Erlemorgaine <erle_3@hotmail.com>
2018-01-18 10:37:53 +0100

    Add error messages to form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -18,7 +18,7 @@ class EventsController < ApplicationController
   def create
     @event = current_user.events.build(event_params)
 
-    if @event.save!
+    if @event.save
       redirect_to @event, notice: "event created"
     else
       render :new
